152d761506e8b096dceed23d5085aba8698991e9
Clarence Mills <clarence.mills@gmail.com>
2017-10-14 21:54:46 -0400

    Condition checking if the ami was entered

--- control_ami.sh
+++ control_ami.sh
@@ -3,6 +3,17 @@
 if [ -z "$1" ];then
 	echo "Pass in ami-id"
 else
-	aws ec2 describe-images --image-id $1 | grep -i snapshotid
+	snapshotid=$(aws ec2 describe-images --image-id $1 | grep -i snapshotid)
+	echo "To delete snapshot issue command: aws ec2 delete-snapshot --snapshot-id $snapshotid"
+        echo "Do you want to delete image $1 y/n"
+	read answer
+	if [ -z "$answer" ]; then
+		echo "You did not answer"
+	else
+		if [ "$answer" == "Y" -o "$answer" ];then
+			aws ec2 deregister-image --image-id $1
+		else
+			echo  "You answered No ... exiting"; exit 0
+		fi
+	fi 
 fi
-
