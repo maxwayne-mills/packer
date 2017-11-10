1e4637d200f264ae54e6a99aa6370f75dd5b2495
Clarence Mills <clarence.mills@gmail.com>
2017-11-10 02:04:12 -0500

    visual changes

--- control_ami.sh
+++ control_ami.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
+clear
 if [ -z "$1" ];then
 	echo "Pass in ami-id"
 else
 	snapshotid=$(aws ec2 describe-images --image-id $1 | grep -i snapshotid)
 	echo "To delete snapshot issue command: aws ec2 delete-snapshot --snapshot-id $snapshotid"
-        echo "Do you want to delete image $1 y/n"
+        echo -n "Do you want to delete image $1 y/n: "
 	read answer
 	if [ -z "$answer" ]; then
 		echo "You did not did not answer yes or no ..."
 		exit 0
 	else
-		if [ "$answer" == "Y" -o "$answer" ];then
+		if [ "$answer" == "Y" -o "$answer" == "y" ];then
 			echo "Deleting $1 ...."
 			aws ec2 deregister-image --image-id $1
 		else
-			echo  "You answered No ... exiting"; exit 0
+			echo  "Not deleting $1 ... "; exit 0
 		fi
 	fi 
 fi
